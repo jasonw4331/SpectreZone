eb56fed4905e1bdd1e636b877d7deec161385991
jasonwynn10 <jasonwynn10@me.com>
2022-05-17 05:03:44 -0400

    Fix implementation of zone expansion

--- src/block/SpectreCoreBlock.php
+++ src/block/SpectreCoreBlock.php
@@ -9,36 +9,35 @@ use pocketmine\item\Item;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
 use pocketmine\world\format\Chunk;
-use twistedasylummc\customies\block\CustomiesBlockFactory;
 
 class SpectreCoreBlock extends Bedrock{
+
 	public function getLightLevel() : int{
 		return 15;
 	}
 
 	public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null) : bool{
-		if($item instanceof Ectoplasm) {
-			$item->pop();
+		if(!$item instanceof Ectoplasm) {
+			return false;
+		}
 
-			$position = $this->getPosition();
+		$position = $this->getPosition();
+		$world = $position->getWorld();
 
-			$chunk = $this->getPosition()->getWorld()->getChunk(
-				$position->getFloorX() >> Chunk::COORD_BIT_SIZE,
-				$position->getFloorZ() >> Chunk::COORD_BIT_SIZE
-			);
+		$cornerX = ($position->getFloorX() >> Chunk::COORD_BIT_SIZE) << Chunk::COORD_BIT_SIZE;
+		$cornerZ = ($position->getFloorZ() >> Chunk::COORD_BIT_SIZE) << Chunk::COORD_BIT_SIZE;
 
-			$block = CustomiesBlockFactory::getInstance()->get('spectrezone:spectre_block');
-			// find lowest block in chunk above y = 2
-			for($y = 2; $y < $position->getWorld()->getMaxY(); ++$y){
-				if($chunk->getFullBlock(0, $y, 0) === $block->getFullId()) {
-					// remove all blocks at this height
-					for($x = 0; $x <= Chunk::EDGE_LENGTH; ++$x){
-						for($z = 0; $z <= Chunk::EDGE_LENGTH; ++$z){
-							$chunk->setFullBlock($x, $y, $z, VanillaBlocks::AIR()->getFullId());
-						}
+		// find lowest block in chunk above y = 2
+		for($y = 2; $y < $position->getWorld()->getMaxY() - 1; ++$y){
+			if($world->getBlockAt(0, $y, 0) instanceof SpectreBlock) {
+				// remove all blocks at this height
+				for($x = 0; $x <= Chunk::EDGE_LENGTH; ++$x){
+					for($z = 0; $z <= Chunk::EDGE_LENGTH; ++$z){
+						$world->setBlockAt($cornerX + $x, $y, $cornerZ + $z, VanillaBlocks::AIR(), false);
 					}
-					return true;
 				}
+				$item->pop(); // consume one ectoplasm on success
+				return true;
 			}
 		}
 		return false;
